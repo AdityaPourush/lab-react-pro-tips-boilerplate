30d7114d3c5a895966f89200abf4a90cd4fdf962
AdityaPourush <adityapourush11@outlook.com>
2023-12-28 09:28:53 +0530

    responsive as well

--- pro-tips/src/Components/Navbar.jsx
+++ pro-tips/src/Components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = ()=> {
     return(
         <>
-            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", margin: "1vh", backgroundColor: "blue"}}>
+            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", margin: "1vh", backgroundColor: "blue", flexWrap: "wrap", width: "100%"}}>
                 <Link to={"/"} style={{padding : "2vh",  color: "black" , textDecoration: "none"}}><h1>Kalvium</h1></Link>
-                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "wrap"}}>
                     <Link to={"/contact"} style={{padding : "2vh",  color: "black" , textDecoration: "none"}}><h1>Contact</h1></Link>
                     <Link to={"/form"} style={{padding : "2vh", color: "black" , textDecoration: "none"}}> <h1>Registration Form</h1> </Link>
                 </div>
